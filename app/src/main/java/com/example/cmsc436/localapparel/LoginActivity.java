71f56e2d6e31a55ee42a47973cd950000ea73e3d
Ronald Davis <ronnied1222@msn.com>
2018-11-03 19:57:12 -0400

    updates

--- app/src/main/java/com/example/cmsc436/localapparel/LoginActivity.java
+++ app/src/main/java/com/example/cmsc436/localapparel/LoginActivity.java
@@ -1,13 +1,57 @@
 package com.example.cmsc436.localapparel;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailField;
+    private EditText passwordField;
+    private String TAG = "LogInActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.emailField);
+        passwordField = findViewById(R.id.passwordField);
+    }
+
+    public void logInPressed(View view) {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //The sign in was successful we should go to next activity
+                    Log.d(TAG, "Signed in successful");
+                    //Get current user information
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    //go to next activty
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    //Sign in failed
+                    Log.w(TAG, "Sign in failed");
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
